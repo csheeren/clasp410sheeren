46ed3040219c228fa65e0e14fc314d194bc768c0
Claire Sheeren <clairesheeren@MacBook-Pro-733.local>
2024-09-18 16:12:31 -0400

    Dan and I fixed everything

--- Lab1.py
+++ Lab1.py
@@ -9,16 +9,20 @@ To get solution for lab 1: Run these commands:
 >>> blah blah
 
 '''
-
+#import essential libraries:
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
 plt.style.use('fivethirtyeight')
 
 
-def fire_spread(nNorth=3, nEast=3, maxiter=4, pspread=1.0):
+
+
+def fire_spread(nNorth=3, nEast=3, maxiter=4, pbare=0, pspread=1.0, pfire=0, center_square =True):
     '''
-    This function performs a fire/disease spread simultion.
+    This function performs a fire/disease spread scenterimultion.
+
     Parameters
     ==========
     nNorth, nEast : integer, defaults to 3
@@ -28,21 +32,46 @@ def fire_spread(nNorth=3, nEast=3, maxiter=4, pspread=1.0):
         Set the maximum number of iterations including initial condition
     pspread : float, defaults to 1
         Chance fire spreads from 0 to 1 (0 to 100%).
+    pbare: float, defaults to 0
+        Percentage of the non forested cells in the grid (0 to 100%). 
+    center_square: boolean, true if center square is on fire and
+        false otherwise.        
     '''
 
     # Create forest and set initial condition
     forest = np.zeros([maxiter, nNorth, nEast]) + 2
-
+    
     # Set fire! To the center of the forest.
-    istart, jstart = nNorth//2, nEast//2
-    forest[0, istart, jstart] = 3
-
+    #istart, jstart = nNorth//2, nEast//2
+    #forest[0, istart, jstart] = 3
+    ignite = np.random.rand(nNorth, nEast)
+    forest_cmap = ListedColormap(['tan', 'darkgreen', 'crimson'])
     # Plot initial condition
+    
+
+    # Ignite center square only.
+    if center_square is True:
+        forest[0, nNorth//2, nEast//2] = 3
+    else:
+        # Randomly set bare spots using pbare.
+        bare_or_immune = np.random.rand(nNorth,nEast) < pbare
+        forest[0, bare_or_immune] = 1
+
+        # Randomly ignite portions of the forest
+        burning_or_infected = np.random.rand(nNorth, nEast) < pfire 
+
+        forest[0, burning_or_infected] = 3
+
     fig, ax = plt.subplots(1, 1)
-    contour = ax.matshow(forest[0, :, :], vmin=1, vmax=3)
+    contour = ax.pcolor(forest[0, :, :], cmap=forest_cmap, vmin=1, vmax=3)
     ax.set_title(f'Iteration = {0:03d}')
-    plt.colorbar(contour, ax=ax)
-    plt.show()
+    cbar=plt.colorbar(contour, ax=ax, ticks=[1.33, 2, 2.67])
+    cbar.ax.set_yticklabels(['Bare', 'Forested', 'Fire'])
+    ax.set_ylabel('y (km)')
+    ax.set_xlabel('x (km)')
+    fig.savefig(f'fig{0:04d}.png')
+    #plt.show()
+    
 
     # Propagate the solution.
     for k in range(maxiter-1):
@@ -54,26 +83,21 @@ def fire_spread(nNorth=3, nEast=3, maxiter=4, pspread=1.0):
 
         # Burn from north to south:
         doburn = (forest[k, :-1, :] == 3) & (forest[k, 1:, :] == 2) & \
-            (ignite[1:, :] <= pspread)
+            (ignite[:-1, :] <= pspread)
         forest[k+1, 1:, :][doburn] = 3
 
-        # Burn in each cardinal direction.
-        # From south to north:
-        for i in range(1, nNorth):
-            for j in range(nEast):
-                # Is current patch burning AND adjacent forested?
-                if (forest[k, i, j] == 3) & (forest[k, i-1, j] == 2):
-                    # Spread fire to new square:
-                    forest[k+1, i-1, j] = 3
-
+         # Burn from south to north:
+        doburn = (forest[k, 1:, :] == 3) & (forest[k, :-1, :] == 2) & \
+            (ignite[1:, :] <= pspread)
+        forest[k+1, :-1, :][doburn] = 3
+        
         # From east to west
         for i in range(nNorth):
             for j in range(nEast-1):
                 # Is current patch burning AND adjacent forested?
-                if (forest[k, i, j] == 3) & (forest[k, i, j+1] == 2) & \
-                    (np.random.rand() < pspread):
-                        # Spread fire to new square:
-                        forest[k+1, i, j+1] = 3
+                if (forest[k, i, j] == 3) & (forest[k, i, j+1] == 2):
+                    # Spread fire to new square:
+                    forest[k+1, i, j+1] = 3
 
         # From west to east
         for i in range(nNorth):
@@ -86,15 +110,16 @@ def fire_spread(nNorth=3, nEast=3, maxiter=4, pspread=1.0):
         # Set currently burning to bare:
         wasburn = forest[k, :, :] == 3  # Find cells that WERE burning
         forest[k+1, wasburn] = 1       # ...they are NOW bare.
-
+       
+        
         fig, ax = plt.subplots(1, 1)
-        contour = ax.matshow(forest[k+1, :, :], vmin=1, vmax=3)
+        contour = ax.pcolor(forest[k+1, :, :], cmap=forest_cmap, vmin=1, vmax=3)
         ax.set_title(f'Iteration = {k+1:03d}')
-        plt.colorbar(contour, ax=ax)
-        plt.show()
-
-        fig.savefig(f'fig{k:04d}.png')
-        plt.close('all')
+        cbar=plt.colorbar(contour, ax=ax, ticks=[1.33, 2, 2.67])
+        cbar.ax.set_yticklabels(['Bare', 'Forested', 'Fire'])
+        ax.set_ylabel('y (km)')
+        ax.set_xlabel('x (km)')
+        fig.savefig(f'fig{k+1:03d}.png')
 
         # Quit if no spots are on fire.
         nBurn = (forest[k+1, :, :] == 3).sum()
@@ -113,7 +138,19 @@ def explore_burnrate():
 
     for i, p in enumerate(prob):
         print(f"Buring for pspread = {p}")
-        nsteps[i] = fire_spread(nEast=5, pspread=p, maxiter=100)
+        nsteps[i] = fire_spread(nEast=3, pspread=p, maxiter=100)
 
     plt.plot(prob, nsteps)
+
+def explore_bare():
+    '''Vary amount of non-forested cells from 0 to 100%'''
+    
+    prob = np.arange(0, 1, .05)
+    nsteps = np.zeros(prob.size)
+
+
+
+
+
+
 
