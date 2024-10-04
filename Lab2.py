7b5b24f1d0cde272718bb762973a0ebb81e528f0
csheeren <113309558+csheeren@users.noreply.github.com>
2024-10-03 23:42:26 -0400

    Update Lab2.py

--- Lab2.py
+++ Lab2.py
@@ -131,8 +131,9 @@ a=1, b=2, c=1, d=3):
     Solve a single ODE using the Dormand Prince 8th order adaptive RK
     method with dense output (using Scipy's `solve_ivp` function).
 
-    Arguments are the same as `euler` and yes I'm too lazy to retype this
-    right now. No judgement please.
+    Arguments are the same as `euler`:  Given a function representing the first derivative of f, an
+    initial condiition f0 and a timestep, dt, solve for f using the RK method.
+ 
 
     Parameters
     ----------
